bdeb2458559f7804514160342a79ded2a9c785c2
laindell <baklangg20@gmail.com>
2025-01-14 14:38:15 +0200

    Додано до постів сериалізатор автора в якому є id, display_name, profile_url V3

--- backend/users/backends.py
+++ backend/users/backends.py
@@ -25,41 +25,4 @@ class EmailBackend(BaseBackend):
             return None
 
 
-class RecommendationService:
-    @staticmethod
-    def recommend_users(current_user, threshold=0.6):
-        recommended_users = []
 
-        # 1. Схожість хештегів
-        current_user_tags = set(tag.name for tag in current_user.hashtags.all())
-        for user in CustomUser.objects.exclude(id=current_user.id):
-            user_tags = set(tag.name for tag in user.hashtags.all())
-            if len(user_tags) == 0:
-                continue
-            similarity = 1 - jaccard_distance(current_user_tags, user_tags)
-            if similarity >= threshold:
-                recommended_users.append(user)
-
-        # 2. Активність
-        recommended_users.extend(
-            CustomUser.objects.filter(total_likes__gte=current_user.total_likes).exclude(id=current_user.id)
-        )
-
-        # 3. Взаємний пошук
-        recommended_users.extend(
-            CustomUser.objects.filter(
-                subscriptions__in=current_user.subscriptions.all(),
-                subscribers__in=current_user.subscribers.all()
-            ).exclude(id=current_user.id)
-        )
-
-        # 4. Географічний принцип
-        recommended_users.extend(
-            CustomUser.objects.filter(
-                Q(location__icontains=current_user.location)
-            ).exclude(id=current_user.id)
-        )
-
-        # Видалення дублікатів
-        recommended_users = list(set(recommended_users))
-        return recommended_users
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserProfileView, UserLogoutView, UserDeleteView, HashtagView, SubscriptionsView, UserSubscriptionsView, RecommendationView
+from .views import UserRegistrationView, UserLoginView, UserProfileView, UserLogoutView, UserDeleteView, HashtagView, SubscriptionsView, UserSubscriptionsView, RecommendationView, UserProfileDetailView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('login/', UserLoginView.as_view(), name='user-login'),
-    path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('profile/', UserProfileView.as_view(), name='user-profile'), # свій профіль
+    path('users/profile/<int:user_id>/', UserProfileDetailView.as_view(), name='user-profile-detail'), # чужий профіль  
     path('logout/', UserLogoutView.as_view(), name='user-logout'),  # Вихід з акаунту
     path('delete/', UserDeleteView.as_view(), name='user-delete'),  # Видалення акаунту
     path('hashtags/', HashtagView.as_view(), name='hashtag-view'),
