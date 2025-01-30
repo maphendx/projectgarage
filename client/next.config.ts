ee6b9ebca0169f0a3e6f3145df8e7ce53aeb2f4d
laindell <baklangg20@gmail.com>
2025-01-30 18:50:20 +0200

    重寫標籤 V4

--- client/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['localhost'], // Add any other domains as needed
-  },
-};
-
-export default nextConfig;
